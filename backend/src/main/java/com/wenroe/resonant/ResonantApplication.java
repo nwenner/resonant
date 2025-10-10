e15f30b13a7d7dd86812b2220f8da94d0cd61daf
nwenner <nwenner@gmail.com>
2025-10-10 10:16:54 -0400

    Switched to Webflux and successfully powered on.

--- backend/src/main/java/com/wenroe/resonant/ResonantApplication.java
+++ backend/src/main/java/com/wenroe/resonant/ResonantApplication.java
@@ -2,10 +2,10 @@ package com.wenroe.resonant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableR2dbcAuditing  // Changed from EnableJpaAuditing
 public class ResonantApplication {
 
     public static void main(String[] args) {
